82c011a6763bac30107032119e166f2495b16cf5
Anjali <anjalisikarwar10@gmail.com>
2026-01-12 12:41:54 +0530

    Day 6 Greeting Shared Library

---
+++ Day_6/GreetingSolution/GreetingApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using GreetingLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreetingApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your name");
+            string name = Console.ReadLine();
+
+            string greeting = GreetingHelper.GetGreeting(name);
+            Console.WriteLine(greeting);
+        }
+    }
+}
