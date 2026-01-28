0c2782fe15f516890d9a2263e772ef49a9411077
Anjali <anjalisikarwar10@gmail.com>
2026-01-28 12:47:05 +0530

    Added Day 19 First Exercise Polymorphism

---
+++ Day_19/Polymorphism/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace OOPSLearning
+{
+    abstract class Vehicle
+    {
+        public string ModelName { get; set; }
+
+        public Vehicle(string modelName)
+        {
+            ModelName = modelName;
+        }
+        public abstract void Move();
+ 
+        public virtual string GetFuelStatus()
+        {
+            return $"Fuel level is stable.";
+        }
+
+    }
+
+    class ElectricCar : Vehicle
+    {
+        public ElectricCar(string modelName) : base(modelName) { }
+        public override void Move()
+        {
+            Console.WriteLine($"{ModelName} is gliding silently on battery power.");
+        }
+        public override string GetFuelStatus()
+        {
+            return $"{ModelName} battery is at 80%";
+        }
+    }
+
+    class HeavyTruck : Vehicle
+    {
+        public HeavyTruck(string modelName) : base(modelName)
+        {
+        }
+
+        public override void Move()
+        {
+            Console.WriteLine($"{ModelName} is hauling cargo with high-torque diesel power.");
+        }
+    }
+
+    class CargoPlane: Vehicle
+    {
+        public CargoPlane(string modelName) : base(modelName)
+        {
+        }
+
+        public override void Move()
+        {
+            Console.WriteLine($"{ModelName} is ascending to 30,000 feet.");
+        }
+        public override string GetFuelStatus()
+        {
+            return base.GetFuelStatus() + " Checking jet fuel reserves...";
+        }
+    }
+
+     internal class Polymorphism
+     {
+        static void Main(string[] args)
+        {
+            Vehicle[] fleet = new Vehicle[3];
+            fleet[0] = new ElectricCar("Tesla");
+            fleet[1] = new HeavyTruck("Volvo");
+            fleet[2] = new CargoPlane("Boeing");
+            
+
+            for(int i=0;i<fleet.Length;i++)
+            {
+                fleet[i].Move();
+                Console.WriteLine(fleet[i].GetFuelStatus());
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+
