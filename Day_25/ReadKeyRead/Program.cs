4f34eaec4ab7df0944a27aecb8bf77358fe0c18e
Anjali <anjalisikarwar10@gmail.com>
2026-02-04 14:43:54 +0530

    Added Day 25 Read Key

---
+++ Day_25/ReadKeyRead/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    internal class SecureTerminal
+    {
+        static void Main(string[] args)
+        {
+            string pin = "";
+            Console.Write("Enter 4-digit PIN: ");
+            while (pin.Length < 4)
+            {
+                ConsoleKeyInfo key= Console.ReadKey(true);
+                if (char.IsDigit(key.KeyChar))
+                {
+                    pin += key.KeyChar;
+                    Console.Write("*");
+                }
+                else if(key.Key==ConsoleKey.Backspace && pin.Length > 0)
+                {
+                    pin = pin.Substring(0, pin.Length - 1);
+                    Console.Write("\b \b");
+                }
+            }
+            Console.WriteLine("\nPin Entered successfully ");
+            Console.WriteLine(pin);
+        }
+    }
+}
+
