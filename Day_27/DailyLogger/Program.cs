9efba073d588a99776077632b1c5c5630614240c
Anjali <anjalisikarwar10@gmail.com>
2026-02-06 15:00:16 +0530

    Added Day 27 Exercise 1 Daily Logger

---
+++ Day_27/DailyLogger/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    internal class DailyReflection
+    {
+        static void Main(string[] args)
+        {
+            string directory = @"..\..\..\";
+            if (!Directory.Exists(directory))
+            {
+                Console.WriteLine("Directory doesn't exits");
+                return;
+            }
+            string file = directory + @"journal.txt";
+            using StreamWriter sw= new StreamWriter(file,true);
+           
+            Console.WriteLine("Enter Daily Reflection");
+            string data = Console.ReadLine();
+            sw.WriteLine(data);
+
+        }
+    }
+}
+
