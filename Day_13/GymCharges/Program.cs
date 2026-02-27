4dbc0c3c16f6b710144ef3bb905c3bfca972495d
Anjali <anjalisikarwar10@gmail.com>
2026-02-27 14:46:35 +0530

    Added Day 13 Exercise

---
+++ Day_13/GymCharges/Program.cs
@@ -0,0 +1,48 @@
+﻿
+namespace OOPSLearning
+{
+    internal class GymCharges
+    {
+        public static double CalculateGymMembership(bool treadmill, bool weightLifting, bool zumba)
+        {
+            double total = 1000; // Fixed monthly charge
+
+            int serviceCount = 0;
+
+            if (treadmill)
+            {
+                total += 300;
+                serviceCount++;
+            }
+
+            if (weightLifting)
+            {
+                total += 500;
+                serviceCount++;
+            }
+
+            if (zumba)
+            {
+                total += 250;
+                serviceCount++;
+            }
+
+            // If no service selected
+            if (serviceCount == 0)
+            {
+                total += 200;
+            }
+
+            // Add GST 5%
+            total += total * 0.05;
+
+            return total;
+        }
+        static void Main(string[] args)
+        {
+            double amount = CalculateGymMembership(true, false, true);
+            Console.WriteLine($"Total Amount: {amount}");
+        }
+    }
+}
+
---
+++ Day_13/LineMethod/Program.cs
@@ -0,0 +1,29 @@
+﻿
+namespace OOPSLearning
+{
+    internal class LineMethod
+    {
+        public static void DisplayLine()
+        {
+            Console.WriteLine(new string('-', 40));
+        }
+
+        public static void DisplayLine(char ch)
+        {
+            Console.WriteLine(new string(ch, 40));
+        }
+
+        public static void DisplayLine(char ch, int count)
+        {
+            Console.WriteLine(new string(ch, count));
+        }
+        static void Main(string[] args)
+        {
+            DisplayLine();
+            DisplayLine('+');
+            DisplayLine('$', 60);
+        }
+    }
+    
+}
+
