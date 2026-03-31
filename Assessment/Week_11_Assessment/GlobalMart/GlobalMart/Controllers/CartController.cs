16f4095bf96e63220e98565c3d51fd92b2e0e654
Anjali <anjalisikarwar10@gmail.com>
2026-03-31 12:40:31 +0530

    Added week 11 assessment

---
+++ Assessment/Week_11_Assessment/GlobalMart/GlobalMart/Controllers/CartController.cs
@@ -0,0 +1,25 @@
+﻿using GlobalMart.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GlobalMart.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly IPricingService _pricingService;
+
+        public CartController(IPricingService pricingService)
+        {
+            _pricingService = pricingService;
+        }
+
+        public IActionResult Checkout(decimal basePrice, string promoCode, decimal finalPrice)
+        {
+
+            ViewBag.BasePrice = basePrice;
+            ViewBag.PromoCode = promoCode;
+            ViewBag.FinalPrice = finalPrice;
+
+            return View();
+        }
+    }
+}
---
+++ Assessment/Week_11_Assessment/GlobalMart/GlobalMart/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using GlobalMart.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GlobalMart.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IPricingService _pricingService;
+
+        public ProductsController(IPricingService pricingService)
+        {
+            _pricingService = pricingService;
+        }
+
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // POST
+        [HttpPost]
+        public IActionResult Index(decimal basePrice, string promoCode)
+        {
+            decimal finalPrice = _pricingService.CalculatePrice(basePrice, promoCode);
+
+            return RedirectToAction("Checkout", "Cart", new
+            {
+                basePrice = basePrice,
+                promoCode = promoCode,
+                finalPrice = finalPrice
+            });
+        }
+    }
+}
