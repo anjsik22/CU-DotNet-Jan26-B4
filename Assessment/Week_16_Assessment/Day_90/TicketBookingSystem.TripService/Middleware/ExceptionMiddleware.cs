dcd3366a7e9a1af94c5ae1160464c6cbfe9d4ba5
Anjali <anjalisikarwar10@gmail.com>
2026-04-25 11:46:57 +0530

    Added updated changes

---
+++ Assessment/Week_16_Assessment/Day_90/TicketBookingSystem.TripService/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,77 @@
+﻿using FluentValidation;
+using System.Net;
+using System.Text.Json;
+using TicketBookingSystem.TripService.Exceptions;
+
+namespace TicketBookingSystem.TripService.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+        private static async Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            context.Response.ContentType = "application/json";
+
+            int statusCode;
+            object response;
+
+            switch (ex)
+            {
+                // 🔴 Your custom exceptions
+                case NotFoundException:
+                    statusCode = (int)HttpStatusCode.NotFound;
+                    response = new { message = ex.Message };
+                    break;
+
+                case BadRequestException:
+                    statusCode = (int)HttpStatusCode.BadRequest;
+                    response = new { message = ex.Message };
+                    break;
+
+                case ValidationException validationEx:
+                    statusCode = (int)HttpStatusCode.BadRequest;
+
+                    var errors = validationEx.Errors
+                        .GroupBy(e => e.PropertyName)
+                        .ToDictionary(
+                            g => g.Key,
+                            g => g.Select(e => e.ErrorMessage).ToArray()
+                        );
+
+                    response = new
+                    {
+                        message = "Validation failed",
+                        errors
+                    };
+                    break;
+
+                // 🔴 Default
+                default:
+                    statusCode = (int)HttpStatusCode.InternalServerError;
+                    response = new { message = "Internal Server Error" };
+                    break;
+            }
+
+            context.Response.StatusCode = statusCode;
+            await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+
+        }
+    }
+}
---
+++ Assignment/Day_90/Day_90/TicketBookingSystem.TripService/Validators/CreateTripValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using TicketBookingSystem.TripService.DTOs;
+
+namespace TicketBookingSystem.TripService.Validators
+{
+    public class CreateTripValidator:AbstractValidator<CreateTripDto>
+    {
+        public CreateTripValidator()
+        {
+            RuleFor(x => x.Heading)
+                .NotEmpty().WithMessage("Heading is required");
+
+            RuleFor(x => x.ShipName)
+                .NotEmpty().WithMessage("Ship name is required");
+
+            RuleFor(x => x.Nights)
+                .GreaterThan(0).WithMessage("Nights must be greater than 0");
+
+            RuleFor(x => x.Price)
+                .GreaterThan(40000).WithMessage("Price must be greater than 40000");
+
+            RuleFor(x => x.StartDate)
+                .LessThan(x => x.EndDate)
+                .WithMessage("StartDate must be before EndDate");
+
+            RuleFor(x => x.Ports)
+                .NotEmpty().WithMessage("At least one port is required");
+        }
+    }
+}
